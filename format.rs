c102f9ee5231d966c603b6180251c7d222ea28e9
Christian Sakai <christianmsakai@gmail.com>
2018-07-04 23:13:33 -0400

    Commit

--- format.rs
+++ format.rs
@@ -11,4 +11,17 @@ fn main() {
     println!("{} of {:b} people know binary, the other half doesn't",  1, 2);
 
     println!("{number:>width$}", number=1, width=6);
+
+    println!("{number:>0width$}", number=1, width=6);
+
+    println!("My name is {0}, {1} {0}", "Bond", "James");
+    
+    #[allow(dead_code)]
+    struct Structure(i32);
+
+    // println!("This struct `{}` won't print...", Structure(3));
+
+    let pi = 3.141592;
+    let formatted_number = format!("{:.*}", 3, pi);
+    println!("Pi is roughly {}", formatted_number);
 }
